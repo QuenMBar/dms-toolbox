d016269c1e05f23a7ef9f78f546245f6c1ab4cc0
Roman Turner <rn.turner@live.com>
2021-04-20 19:46:11 -0700

    mo routes, mo problems

--- the-dms-toolbox/src/App.js
+++ the-dms-toolbox/src/App.js
@@ -4,47 +4,46 @@ import "./App.css";
 import DmPage from "./components/DmPage";
 import LoginPage from "./components/LoginPage";
 
-
 class App extends Component {
-    state = {
-        dmId: null,
-        name: null,
-    };
+  state = {
+    dmId: undefined,
+    name: undefined,
+  };
 
-    setDmIdAndName = (dmId, name) => {
-        this.setState({
-            dmId: dmId,
-            name: name,
-        });
-    };
+  setDmIdAndName = (dmId, name) => {
+    this.setState({
+      dmId: dmId,
+      name: name,
+    });
+  };
 
-    render() {
-        return (
-          <Router>
-              <div className='App'>
-                {/* localhost:3000/dm/:campaign_id/:subpages */}
-                <Route
-                  exact
-                  path='/'
-                  render={(routerProps) => (
-                    <LoginPage
-                      {...routerProps}
-                      setDmIdAndName={this.setDmIdAndName}
-                      dmId={this.state.dmId}
-                    />
-                  )}
-                />
-                <Route
-                  exact
-                  path='/dm'
-                  render={(routerProps) => (
-                    <DmPage {...routerProps} dmId={this.state.dmId} />
-                  )}
-                />
-              </div>         
-          </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          {/* localhost:3000/dm/:campaign_id/:subpages */}
+          <Route
+            exact
+            path='/'
+            render={(routerProps) => (
+              <LoginPage
+                {...routerProps}
+                setDmIdAndName={this.setDmIdAndName}
+                dmId={this.state.dmId}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/dm'
+            render={(routerProps) => (
+              <DmPage {...routerProps} dmId={this.state.dmId} />
+            )}
+          />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- the-dms-toolbox/src/components/DmPage.js
+++ the-dms-toolbox/src/components/DmPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Route, Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 //pages
 import CampaignPage from "./CampaignPage";
 import Navbar from "./Navbar";
@@ -9,32 +9,47 @@ import Error from "./Error";
 const URL = "http://localhost:9393/dm/";
 
 export default class DmPage extends Component {
-
-  state = {
-    id: null,
-    camps: []
-  };
-
-  componentDidMount() {
-    this.setState({ id: this.props.id }, this.getDM());
+  constructor(props) {
+    super(props);
+    console.log(props.id);
+    if (props.id === undefined) {
+      // Fail/Refdirect to Login
+      this.state = {
+        id: undefined,
+        camps: [],
+      };
+    } else {
+      this.state = {
+        id: props.id,
+        camps: [],
+      };
+    }
   }
 
-  setId = () => this.setState({id: this.props.dmId})
-  
+  componentDidMount() {
+    if (this.state.id !== undefined) {
+      this.getDM();
+    }
+  }
+
+  setId = () => this.setState({ id: this.props.dmId });
 
   getDM = () => {
-       fetch(`${URL}${this.state.id}`)
-         .then((res) => res.json())
-         .then((data) =>
-           this.setState({
-             camps: data,
-           })
-         )
-         .catch((e) => console.error("e:", e));
+    fetch(`${URL}${this.state.id}`)
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          camps: data,
+        })
+      )
+      .catch((e) => console.error("e:", e));
+  };
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(prevProps, prevState, snapshot);
   }
 
   render() {
-
     //Todo[]make list prettier
     return (
       <>
