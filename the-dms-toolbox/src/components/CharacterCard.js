f7ca4ef5cb467e70462d16b50b87d523ca226898
QuenMBar <Qbeast360@gmail.com>
2021-04-21 11:28:29 -0700

    Temp

--- the-dms-toolbox/src/components/CharacterCard.js
+++ the-dms-toolbox/src/components/CharacterCard.js
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Collapse from "@material-ui/core/Collapse";
@@ -14,14 +13,11 @@ import { red } from "@material-ui/core/colors";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import ShareIcon from "@material-ui/icons/Share";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -54,11 +50,11 @@ function createData(stat, modifier, raw) {
 }
 
 const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-    createData("Eclair", 262, 16.0, 24, 6.0),
-    createData("Cupcake", 305, 3.7, 67, 4.3),
-    createData("Gingerbread", 356, 16.0, 49, 3.9),
+    // createData(props.char),
+    // createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    // createData("Eclair", 262, 16.0, 24, 6.0),
+    // createData("Cupcake", 305, 3.7, 67, 4.3),
+    // createData("Gingerbread", 356, 16.0, 49, 3.9),
 ];
 
 export default function RecipeReviewCard(props) {
@@ -98,10 +94,10 @@ export default function RecipeReviewCard(props) {
                         {rows.map((row) => (
                             <TableRow key={row.name}>
                                 <TableCell component="th" scope="row">
-                                    {row.name}
+                                    {row.stat}
                                 </TableCell>
-                                <TableCell align="right">{row.calories}</TableCell>
-                                <TableCell align="right">{row.fat}</TableCell>
+                                <TableCell align="right">{row.modifier}</TableCell>
+                                <TableCell align="right">{row.raw}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
